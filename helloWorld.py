365de22d54b5b11c6c20f7d74e41b5ef08e310e5
chatsim79 <command?>
2022-12-11 15:26:10 +0000

    created function and added function documentation

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,12 @@
-# Request user input for text to be printed.
-display = input("Please enter something to be printed/displayed.\n: ")
-# Prints entered text.
-print(display)
+def display_input():
+    """
+    Function will take user input and display
+    on screen for user, to be used as input
+    check in main codebase
+    """
+    # Request user input for text to be printed.
+    display = input("Please enter something to be printed/displayed.\n: ")
+    # Prints entered text.
+    print(display)
+    
+display_input()
